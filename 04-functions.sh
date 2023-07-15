0853910ff8df970604cf881894fd8a71ad33f0d3
sulaimonkhan <134831524+sulaimonkhan@users.noreply.github.com>
2023-07-15 15:29:56 +0530

    Create 04-functions.sh

---
+++ 04-functions.sh
@@ -0,0 +1,14 @@
+print_hello() {
+  echo Hello World
+}
+
+print_hello
+
+print_arguments() {
+  echo First Argument $1
+  echo Second Argument $2
+  echo All Arguments - $*
+  echo No of Argumetns - $#
+}
+
+print_arguments abc 123 x
