0189f5c7dbe908a915cf55a8ce5f7f4d1f5cd963
sulaimonkhan <134831524+sulaimonkhan@users.noreply.github.com>
2023-07-15 15:29:06 +0530

    Create 03-vars.sh

---
+++ 03-vars.sh
@@ -0,0 +1,14 @@
+a=10
+
+echo Value of a is $a dollars
+
+# Alternatively variables also can be accessed by using ${a}
+echo Value of a is ${a}dollars
+
+
+# Special variables for inputs
+# Special variables used for inputs are $0 to $n, $* , $#
+echo Script Name - $0
+echo First Argument - $1
+echo All Arguments - $*
+echo No of Arguments - $#
